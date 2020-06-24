0db587d91234e165472db985ead2d187c0e54490
wordaddict <madeyinka6@gmail.com>
2020-06-24 22:19:15 +0100

    fix conflicts

--- app/route/index.js
+++ app/route/index.js
@@ -6,14 +6,9 @@ const AuthController = require('../controllers/auth');
 
 // company (route)
 router.use(bodyParser.json());
-router.post('/login', (req, res) => {
-    return new AuthController().login(req, res);
-});
-
 router.post('/add-company', (req, res) => {
     return new companyController().inviteCompany(req, res);
 });
-
 
 router.get('/:companyEmail', (req, res) => {
     return new onboardingController().updateCompanyRecordByEmail(req, res);
@@ -31,38 +26,8 @@ router.post('/resendEmail', (req, res) => {
     return new AuthController().resendEmail(req, res)
 })
 
-
 router.get('/:companyId', (req, res) => {
     return new onboardingController().updateCompanyRecordById(req, res);
 });
 
-
-
-
-
-
 module.exports = router;
-
-
-
-
-
-
-
-
-
-// route.post('/update', (req, res) => {
-//     return new companyController().updateCompany(req, res)
-// })
-
-
-
-
-// const onboardingRouter = require('express').Router();
-// const MainController = require('../controllers/onboardingController');
-
-// onBoardingRouter.get('/:companyId', (req, res) => {
-//     return new MainController().updateCompanyRecordById(req, res)
-// });
-
-
