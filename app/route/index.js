22d47cdb8d319cd1461e3335f0284c111c8ba04c
wordaddict <madeyinka6@gmail.com>
2020-06-24 22:27:35 +0100

    remove unused code

--- app/route/index.js
+++ app/route/index.js
@@ -10,10 +10,6 @@ router.post('/add-company', (req, res) => {
     return new companyController().inviteCompany(req, res);
 });
 
-router.get('/:companyEmail', (req, res) => {
-    return new onboardingController().updateCompanyRecordByEmail(req, res);
-});
-
 router.post('/login', (req, res) => {
     return new AuthController().login(req, res);
 });
@@ -25,10 +21,6 @@ router.get('/validate', (req, res) => {
 router.post('/resendEmail', (req, res) => {
     return new AuthController().resendEmail(req, res)
 })
-
-router.get('/:companyId', (req, res) => {
-    return new onboardingController().updateCompanyRecordById(req, res);
-});
 
 router.post('/update', (req, res) => {
     return new companyController().updateCompany(req, res)
